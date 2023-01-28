cfabf1a6f27660f06c3515fc05f784a53fd879db
haoxle <hao-le@live.co.uk>
2023-01-28 21:11:54 +0000

    2 challenges 6kyu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,15 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+
+import static java.lang.Double.parseDouble;
 
 public class Main {
     public static int convertToMins(double x) {
         String str = String.format("%.2f", x);
-        System.out.println(str);
         int minutes = 0;
         String[] arr = str.split("[.]");
-        System.out.println(Arrays.toString(arr));
         if (arr.length == 2) {
             minutes = Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
         }
@@ -16,6 +17,32 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(convertToMins(1.30));
+
+
+        String[][] a = new String[][]{{"9:00", "10:00"}, {"11:00", "12:00"}};
+        ArrayList<Double> c = new ArrayList<>();
+        Double[][] b = new Double[a.length][2];
+        Double[] e ;
+
+        for (String[] strings : a) {
+            for (String strings2 : strings) {
+                c.add(Double.parseDouble(strings2.replace(':', '.')));
+            }
+        }
+
+        System.out.println(Arrays.toString(new ArrayList[]{c}));
+
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                b[i][0] = c.get(i);
+                b[i][1] = c.get(j);
+
+            }
+        }
+
+
+
+        System.out.println(Arrays.deepToString(b));
     }
 }
